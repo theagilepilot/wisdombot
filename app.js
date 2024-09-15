3e5269cea839b5b3d855bcded9b9b8d3f94c9195
nicholasamato <nxa588@miami.edu>
2024-09-15 16:32:55 -0400

    Add bold

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), async fun
         type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
         data: {
           // Fetches a random emoji to send from a helper function
-          content: `Prompt: ${message} \nAI Response: ${gptResponse}`
+          content: `**Prompt:** ${message} \n**AI Response:** ${gptResponse}`
         },
       });
     }
